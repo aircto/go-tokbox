6c02f588a3cd5df88014a430ad38abcd50d9c085
Kaviraj <kavirajkanagaraj@gmail.com>
2017-04-18 17:11:32 +0530

    Test for jwt token

--- tokbox_test.go
+++ tokbox_test.go
@@ -7,9 +7,24 @@ const (
 	testSecret = "362f8bfbb5fff2f960c72ee4b798fa7029f9a601"
 )
 
-func TestTokBoxCreateSession(t *testing.T) {
+func TestTokboxCreateSession(t *testing.T) {
 	tok := New(testKey, testSecret)
 	if tok.CreateSession() == nil {
 		t.Errorf("expected non-nil, but got nil")
+	}
+}
+
+func TestTokboxJwtToken(t *testing.T) {
+	tok := New(testKey, testSecret)
+	token1, err := tok.jwtToken()
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+	token2, err := tok.jwtToken()
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+	if token1 == token2 {
+		t.Errorf("expected unique token, got %v, %v", token1, token2)
 	}
 }
